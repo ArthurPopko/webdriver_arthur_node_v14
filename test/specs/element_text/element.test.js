302f2e477174d2a45d7b3d065d1cb285021c37dd
arthur.popko <arthur.popko@icloud.com>
2021-12-31 15:31:17 +0300

    xpath selector added

--- test/specs/element_text/element.test.js
+++ test/specs/element_text/element.test.js
@@ -1,8 +1,23 @@
 describe("Element interaction", function () {
-    it("Element element text console log", () => {
+    it("Element element text css H2 console log", () => {
         browser.url('/')
         let text = $("h2").getText()
-        console.log('Here must be text:')
+        console.log('Here must be a text:')
         console.log(text);
     })
+
+    it("Element element text css HEADING console log", () => {
+        browser.url('/')
+        let text = $(".heading").getText()
+        console.log('Here must be a text:')
+        console.log(text);
+    })
+
+    it("Element element text Xpath FOOTER console log", () => {
+        browser.url('/')
+        let text = $("//*[@id='page-footer']").getText()
+        console.log('Here must be a text:')
+        console.log(text);
+    })
+
 })
