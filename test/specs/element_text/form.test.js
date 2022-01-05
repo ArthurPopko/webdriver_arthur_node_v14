f8940be066283dd30330edcca5bd1c86f89d20a4
QAfun <arthur.popko@icloud.com>
2022-01-05 15:20:30 +0300

    userData from file

--- test/specs/element_text/form.test.js
+++ test/specs/element_text/form.test.js
@@ -1,13 +1,14 @@
 const MainPage = require('../../pageobjects/main.page')
 const FormPage = require('../../pageobjects/form.page')
+const LoginData = require('../../../data/logindata')
 
 describe('Forms interaction', () => {
     it('Login form fill', () => {
         browser.url('/')
         MainPage.specificChildElement(21).click()
 
-        FormPage.enterUsername('tomsmith')
-        FormPage.enterPassword('SuperSecretPassword!')
+        FormPage.enterUsername(LoginData.userName)
+        FormPage.enterPassword(LoginData.password)
         FormPage.loginBtn.click()
         FormPage.flashAlert.waitForDisplayed()
         expect(FormPage.flashAlert.getText()).toEqual('You logged into a secure area!\n×')
